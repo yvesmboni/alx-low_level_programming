50eb74f7857ccc667c0a69a54ad35e7e8bc107df
yvesmboni <yvesmboni250@gmail.com>
2022-10-14 11:12:29 -0700

    Most hackers are young

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 /**
- * main - prints its own opcodes
- * @argc: number of arguments
- * @argv: array of arguments
+ *  main - prints its own opcodes
+ *  @argc: number of arguments
+ *  @argv: array of arguments
  *
- * Return: Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	int bytes, i;
 	char *arr;
 
-	rf (argc != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
@@ -40,4 +40,3 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
-
