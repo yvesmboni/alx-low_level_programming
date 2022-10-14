478bd0b5b2f822d4bbc7f67f63f1c7342acc7fc7
yvesmboni <yvesmboni250@gmail.com>
2022-10-14 11:29:04 -0700

    A goal is not always meant to be reached

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -38,10 +38,10 @@ int op_mul(int a, int b)
 
 /**
  * op_div - calculates the division of two integers
- * @a: first integer
- * @b: second integer
+ *  @a: first integer
+ *  @b: second integer
  *
- * Return: result of the division of a and b
+ *  Return: result of the division of a and b
  */
 int op_div(int a, int b)
 {
