5bbc4439af9f7c28f8a77fc1a93a712aa92595fa
yvesmboni <yvesmboni250@gmail.com>
2022-12-12 10:18:16 -0800

    task 0

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("main.h"));
+	return (EXIT_SUCCESS);
+}
